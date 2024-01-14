1132a4856164e5b687949e18589c0ae859c46a43
ajay26188 <ajaysah52@gmail.com>
2024-01-14 15:39:45 +0200

    last commit

--- routes/index.js
+++ routes/index.js
@@ -53,7 +53,7 @@ router.post('/api/user/register',
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({errors: errors.array()});
+      return res.status(403).json({errors: errors.array()});
     }
 
     User.findOne({ username })
